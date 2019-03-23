f00541469ac17b1c4cd678246f7a4a9b4f348244
Jose Elera <jelera@gmail.com>
2019-03-23 01:27:09 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
  # your code here!
+  output = []
+
+  i = 0
+  while i < collection
+
+    is_item_selected = yield(collection[i])
+
+    output << is_item_selected if is_item_selected
+
+    i += 1
+  end
+
+  output
 end
