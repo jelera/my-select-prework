1aa0516127579d9b18f9a42879340ce7dabb59bf
Jose Elera <jelera@gmail.com>
2019-03-23 01:31:57 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,23 +1,19 @@
 def my_select(collection)
  # your code here!
-  if collection.empty?
-    raise "This Block should not run!"
-  else
 
-    output = []
+  output = []
 
-    i = 0
-    while i < collection
+  i = 0
+  while i < collection.length
 
-      is_item_selected = yield(collection[i])
+    is_item_selected = yield(collection[i])
 
-      output << is_item_selected if is_item_selected
+    output << is_item_selected if is_item_selected
 
-      i += 1
-    end
-
-    output
-
-
+    i += 1
   end
+
+  output
+
+
 end
